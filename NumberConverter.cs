b38d08dfa7e013f2690790de5a76cffb3e3feab8
Robert Woods <robert.woods@fulbrightmail.org>
2023-03-20 16:31:08 -0400

    initial commit

---
+++ NumberConverter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_Numeral_Converter___Algorithm_Practice
+{
+    internal class NumberConverter
+    {
+        public string ConvertToRomanNum(int input)
+        {
+
+            string[] ones = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+            string[] tens = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+
+            string[] hundreds = { "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+
+            string[] thousands = { "M", "MM", "MMM", "MMMM", "MMMMM", "MMMMMM", "MMMMMMM", "MMMMMMMM", "MMMMMMMMM" };
+
+
+            string output = "";
+
+            string inputString = input.ToString();
+
+
+            if (inputString.Length == 1)
+            {
+                output += ones[input - 1];
+            }
+            else if (inputString.Length == 2)
+            {
+                int firstDigit = (int)Char.GetNumericValue(inputString[0]);
+                output += tens[firstDigit - 1];
+
+                int secondDigit = (int)Char.GetNumericValue(inputString[1]);
+
+                if(secondDigit != 0)
+                {
+                    output += ones[secondDigit - 1];
+                } 
+                
+            }
+            else if (inputString.Length == 3)
+            {
+                int firstDigit = (int)Char.GetNumericValue(inputString[0]);
+                output += hundreds[firstDigit - 1];
+
+                int secondDigit = (int)Char.GetNumericValue(inputString[1]);
+
+                if (secondDigit != 0)
+                {
+                    output += tens[secondDigit - 1];
+                }
+
+                int thirdDigit = (int)Char.GetNumericValue(inputString[2]);
+                
+                if (thirdDigit != 0)
+                {
+                    output += ones[thirdDigit - 1];
+                }
+
+            }
+            else if (inputString.Length == 4)
+            {
+                int firstDigit = (int)Char.GetNumericValue(inputString[0]);
+                output += thousands[firstDigit - 1];
+
+                int secondDigit = (int)Char.GetNumericValue(inputString[1]);
+
+                if (secondDigit != 0)
+                {
+                    output += hundreds[secondDigit - 1];
+                }
+
+                int thirdDigit = (int)Char.GetNumericValue(inputString[2]);
+
+                if(thirdDigit != 0)
+                {
+                    output += tens[thirdDigit - 1];
+                } 
+
+                int fourthDigit = (int)Char.GetNumericValue(inputString[3]);
+
+                if(fourthDigit != 0)
+                {
+                    output += ones[fourthDigit - 1];
+                }           
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Roman_Numeral_Converter___Algorithm_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //GOAL: Create a method that takes in an integer from the user, then converts that integer to its Roman numeral equivalent, up to a max of 10,000.
+            //Note: The data type of the resultant numeral is a string.
+
+            //1) Create a dictionary, or group of lists for each tens place, which binds the values of the Roman symbols to their numeric counterparts.
+
+            //2) Convert the user's numeric input into a string.
+            //3) Check the length of the user's input. Given the length, this will determine the Numeral needed to start (i.e. 4 digits = thousands, 3 digits = hundreds);
+            //4) From left to right, add in the characters that would represent the necessary amount.
+            //5) Display the resultant output string to the user.
+
+            bool goOn = true;
+
+            while (goOn)
+            {
+                NumberConverter n = new NumberConverter();
+
+                Console.WriteLine("Please enter a non-negative whole number that you'd like to convert into Roman Numerals:\n");
+
+                int input = int.Parse(Console.ReadLine());
+
+                string output = n.ConvertToRomanNum(input);
+
+                Console.WriteLine($"\nYour number in Roman Numerals is: \n\n{output}\n");
+
+                goOn = AskToGoOn();
+            }
+
+        }
+
+        public static bool AskToGoOn()
+        {
+            Console.WriteLine("\nWould you like convert another number? Please enter yes (Y) or no (N).\n");
+
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "Y" || input == "y" || input == "yes")
+            {
+                Console.WriteLine("\nAlright! Let's convert another number!\n");
+                return true;
+            }
+            else if (input == "N" || input == "n" || input == "no")
+            {
+                Console.WriteLine("\nGoodbye!\n");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("\nI'm sorry. I didn't understand that. Please try again.\n");
+                return AskToGoOn();
+            }
+        }
+
+    }
+}
